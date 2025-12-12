0677b8e5b77d34c8ee04d34b4c3f6225ca7efefb
Dylan Frendo <dylanfrendo@gmail.com>
2025-12-12 21:36:51 +0700

    added config

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,17 @@
 import fs from 'fs';
 import yaml from 'yaml';
 import { BotConfig } from './types/config';
+import { OrderType } from '../lib/rest-api/types';
 
 export function loadConfig(path: string = 'config.yaml'): BotConfig {
   const file = fs.readFileSync(path, 'utf8');
-  return yaml.parse(file) as BotConfig;
+  const configs =  yaml.parse(file) as BotConfig;
+
+  if (![OrderType.Spot, OrderType.FillOrKill].includes(configs.o2.market.order_type)) {
+    throw new Error(
+      `Invalid order_type: ${configs.o2.market.order_type}. Must be either 'Spot' or 'FillOrKill'.`
+    );
+  }
+
+  return configs;
 }
